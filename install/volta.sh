dab939410f3bf206d863641eadfb7490d6867f08
dragutesku <alexandru.dragutescu@gmail.com>
2023-05-16 20:23:58 +0000

    i volta'd the packages with npm :))

--- install/volta.sh
+++ install/volta.sh
@@ -2,11 +2,11 @@
 # https://volta.sh/
 volta install node;
 volta install npm;
-volta install -g typescript;
-volta install -g eslint;
-volta install -g http-server;
-volta install -g nodemon;
-volta install -g @angular/cli;
-volta install -g next@latest;
-volta install -g react@latest;
-volta install -g react-dom@latest;
+volta install typescript;
+volta install eslint;
+volta install http-server;
+volta install nodemon;
+volta install @angular/cli;
+volta install next@latest;
+volta install react@latest;
+volta install react-dom@latest;
